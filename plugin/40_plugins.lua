44d57c59ca51a7f69a44989fd66f951a2aabd5d9
erudhir101 <bieusc@hotmail.com>
2026-01-26 14:52:23 -0300

    refactor: return flash to default

--- plugin/40_plugins.lua
+++ plugin/40_plugins.lua
@@ -41,47 +41,8 @@ later(function()
     },
   })
 
-  local function format(opts)
-    return { { opts.match.label1, "FlashMatch" },
-      { opts.match.label2, "FlashLabel" }
-    }
-  end
-
-  local opts = {
-    search = { mode = "search" },
-    label = { after = false, before = { 0, 0 }, uppercase = false, format = format },
-    pattern = [[\<]],
-    action = function(match, state)
-      state:hide()
-      flash.jump({
-        search = { max_length = 0 },
-        highlight = { matches = false },
-        label = { format = format },
-        matcher = function(win)
-          -- limit matches to the current label
-          return vim.tbl_filter(function(m)
-            return m.label == match.label and m.win == win
-          end, state.results)
-        end,
-        labeler = function(matches)
-          for _, m in ipairs(matches) do
-            m.label = m.label2 -- use the second label
-          end
-        end,
-      })
-    end,
-    labeler = function(matches, state)
-      local labels = state:labels()
-      for m, match in ipairs(matches) do
-        match.label1 = labels[math.floor((m - 1) / #labels) + 1]
-        match.label2 = labels[(m - 1) % #labels + 1]
-        match.label = match.label1
-      end
-    end,
-  }
-
   vim.keymap.set({ "n", "x", "o" }, ";f", function()
-    flash.jump(opts)
+    flash.jump()
   end, { desc = "flash Jump" })
   vim.keymap.set({ "n", "x", "o" }, ";s", function()
     flash.treesitter()
